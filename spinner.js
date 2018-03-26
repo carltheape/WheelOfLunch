fbec71a6bfba4c60562a977e1d21c23f831741d0
RIGHTPOINT\TFinkle <tfinkle@rightpoint.com>
2018-03-26 14:35:13 -0400

    browser checking

--- spinner.js
+++ spinner.js
@@ -1,6 +1,26 @@
 var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+// Opera 8.0+
+var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+
+// Firefox 1.0+
+var isFirefox = typeof InstallTrigger !== 'undefined';
+
+// Safari 3.0+ "[object HTMLElementConstructor]" 
+var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+
+// Internet Explorer 6-11
+var isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+// Edge 20+
+var isEdge = !isIE && !!window.StyleMedia;
+
+// Chrome 1+
+var isChrome = !!window.chrome && !!window.chrome.webstore;
+
+// Blink engine detection
+var isBlink = (isChrome || isOpera) && !!window.CSS;
 
 // var stuff = ["kfc", "mcd", "king"];
 // var restaurants = JSON.parse(localStorage.getItem("restaurants"));
@@ -30,7 +50,10 @@ let getRandomColor = function() {
         color += letters[Math.floor(Math.random() * 16)];
     }
     newColor = color;
-    return color+"80";
+    if(isIE || isEdge){
+        return color+"80";
+    }
+   else{return color+"80";}
 
 };
 
